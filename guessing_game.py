c649f8c8f3d24cbee73e55adea3df140bad72798
agnesrausz <rauszka94@gmail.com>
2020-11-23 03:22:45 +0100

    add code comprehension comments

--- guessing_game.py
+++ guessing_game.py
@@ -1,6 +1,6 @@
 import math
 import random
-a = []
+a = []   # random numbers, next 10 line append empty list with 10 random number 1-99
 a.append(random.randint(1, 99))
 a.append(random.randint(1, 99))
 a.append(random.randint(1, 99))
@@ -11,19 +11,19 @@ a.append(random.randint(1, 99))
 a.append(random.randint(1, 99))
 a.append(random.randint(1, 99))
 a.append(random.randint(1, 99))
-for i in range(10):
-    g = int(input("Enter an integer from 1 to 99: "))
-    while a[i] != g:
-        if g < a[i]:
+for i in range(10):   # for loop 10x
+    g = int(input("Enter an integer from 1 to 99: "))   # ask number
+    while a[i] != g:   # while loop till random number != asked number
+        if g < a[i]:   # next 5 line ask numbers if it not the random number
             print("guess is low")
             g = int(input("Enter an integer from 1 to 99: "))
         elif g > a[i]:
             print("guess is high")
             g = int(input("Enter an integer from 1 to 99: "))
-        else:
+        else:   # break the loop if the number is guessed
             break
-    print("you guessed it!")
-
+    print("you guessed it!")   # pirt you guessed
+# the same with 1-49
 b = []
 b.append(random.randint(1, 49))
 b.append(random.randint(1, 49))
