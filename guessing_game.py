01756061e79548d1461510418eb8ed20ab53c309
agnesrausz <rauszka94@gmail.com>
2020-11-23 04:27:47 +0100

    add meaningful names, remove unnecessary code, remove duplicating code

--- guessing_game.py
+++ guessing_game.py
@@ -1,49 +1,16 @@
-import math
 import random
-a = []   # random numbers, next 10 line append empty list with 10 random number 1-99
-a.append(random.randint(1, 99))
-a.append(random.randint(1, 99))
-a.append(random.randint(1, 99))
-a.append(random.randint(1, 99))
-a.append(random.randint(1, 99))
-a.append(random.randint(1, 99))
-a.append(random.randint(1, 99))
-a.append(random.randint(1, 99))
-a.append(random.randint(1, 99))
-a.append(random.randint(1, 99))
-for i in range(10):   # for loop 10x
-    g = int(input("Enter an integer from 1 to 99: "))   # ask number
-    while a[i] != g:   # while loop till random number != asked number
-        if g < a[i]:   # next 5 line ask numbers if it not the random number
-            print("guess is low")
-            g = int(input("Enter an integer from 1 to 99: "))
-        elif g > a[i]:
-            print("guess is high")
-            g = int(input("Enter an integer from 1 to 99: "))
-        else:   # break the loop if the number is guessed
-            break
-    print("you guessed it!")   # pirt you guessed
-# the same with 1-49
-b = []
-b.append(random.randint(1, 49))
-b.append(random.randint(1, 49))
-b.append(random.randint(1, 49))
-b.append(random.randint(1, 49))
-b.append(random.randint(1, 49))
-b.append(random.randint(1, 49))
-b.append(random.randint(1, 49))
-b.append(random.randint(1, 49))
-b.append(random.randint(1, 49))
-b.append(random.randint(1, 49))
-for i in range(10):
-    g = int(input("Enter an integer from 1 to 49: "))
-    while b[i] != g:
-        if g < b[i]:
-            print("guess is low")
-            g = int(input("Enter an integer from 1 to 49: "))
-        elif g > b[i]:
-            print("guess is high")
-            g = int(input("Enter an integer from 1 to 49: "))
-        else:
-            break
-    print("you guessed it!")
+
+def guess_the_number(guess_range):
+    for i in range(10):
+        random_num = (random.randint(1, guess_range))
+        guess = int(input("Enter an integer from 1 to "+ str(guess_range) +": "))
+        while random_num != guess:
+            if guess < random_num:
+                print("guess is low")
+            elif guess > random_num:
+                print("guess is high")
+            guess = int(input("Enter an integer from 1 to "+ str(guess_range) +": "))
+        print("you guessed it!")
+
+guess_the_number(99)
+guess_the_number(49)
